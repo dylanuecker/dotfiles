7cb1421336034ed8d7cf519fa36bfa96b1b3326d
Dylan Uecker <dylan.uecker@gmail.com>
2022-08-28 20:35:03 -0400

    Doing slightly more

--- setup.sh
+++ setup.sh
@@ -9,3 +9,11 @@ echo "Copied .vim directory to home directory"
 cp .bashrc ~/
 echo "Copied .bashrc to home directory"
 
+git config --global user.name "Dylan Uecker"
+git config --global user.email dylan.uecker@gmail.com
+git config --global core.editor vim
+echo -e "Bare minimum git configurations made\n"
+
+apt update
+apt upgrade
+
