43e61ca2a2aefc414edca2601d0da47365c8c964
Dylan Uecker <dylan.uecker@gmail.com>
2022-09-02 18:38:39 -0400

    Hide default home folders

--- setup.sh
+++ setup.sh
@@ -1,19 +1,18 @@
-#!/bin/bash
+#!/bin/bash -v
 
 sudo apt update
-echo
-sudo apt upgrade
-echo
-sudo apt install vim -y
-echo
-sudo apt install git -y
-echo
 
-cp -a to_home/. ~/
-echo "Copied to_home to home"
+sudo apt upgrade
+
+sudo apt install vim -y
+
+sudo apt install git -y
+
+cp -a dotfiles/. ~/
+
+mkdir -p ~/.hidden/
+cp settings/user-dirs.dirs ~/.config/
 
 git config --global user.email "dylan.uecker@gmail.com"
 git config --global user.name "Dylan Uecker"
 git config --global core.editor "vim"
-echo "Bare minimum git configurations made"
-
