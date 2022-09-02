a854b3a0ae613c7bbef5698e71a5c7cf8d7b5f64
Dylan Uecker <dylan.uecker@gmail.com>
2022-09-02 18:53:39 -0400

    Hide snap directory

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ cp -a dotfiles/. ~/
 mkdir -p ~/.hidden/
 cp settings/user-dirs.dirs ~/.config/
 
+mv ~/snap/ ~/.snap/
+
 git config --global user.email "dylan.uecker@gmail.com"
 git config --global user.name "Dylan Uecker"
 git config --global core.editor "vim"
