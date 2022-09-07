0c4c3a4007b5644bd33e0e2756c23d5f4b551691
Dylan Uecker <dylan.uecker@gmail.com>
2022-09-07 00:43:53 -0400

    Updated references

---
+++ ubuntu_post_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -v
+
+sudo apt update
+
+sudo apt upgrade
+
+sudo apt install vim -y
+
+sudo apt install git -y
+
+cp -a dotfiles/. ~/
+
+mkdir -p ~/.hidden/
+mv ~/Desktop/ ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/ ~/.hidden/
+cp gnomefiles/user-dirs.dirs ~/.config/
+
+dconf load /org/gnome/terminal/ < gnomefiles/gnome_terminal_settings.txt
+
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
+
+git config --global user.email "dylan.uecker@gmail.com"
+git config --global user.name "Dylan Uecker"
+git config --global core.editor "vim"
