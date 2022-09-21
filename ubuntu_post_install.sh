099c9d5fab47f9933301eb25238b4d88361d4d16
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2022-09-21 12:41:16 -0400

    Added condition checking for when to run certain commands

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,19 +1,29 @@
-#!/bin/bash -v
+#!/bin/bash
 
-sudo apt update
+echo -n "Manage packages[Y/n]? "
+read choice
 
-sudo apt upgrade
+if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+	set -v
+	sudo apt update -y
+	sudo apt upgrade -y
+	sudo apt install vim -y
+	sudo apt install git -y
+	set +v
+fi
 
-sudo apt install vim -y
+if [ ! -d ~/.hidden/ ]; then
+	set -v
+	mkdir ~/.hidden/
+	mv ~/Desktop/ ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/ ~/.hidden/
+	set +v
+fi
 
-sudo apt install git -y
+set -v
 
 cp -a dotfiles/. ~/
 
-mkdir -p ~/.hidden/
-mv ~/Desktop/ ~/Documents/ ~/Downloads/ ~/Music/ ~/Pictures/ ~/Public/ ~/Templates/ ~/Videos/ ~/.hidden/
 cp gnome-files/user-dirs.dirs ~/.config/
-
 dconf load /org/gnome/terminal/ < gnome-files/gnome_terminal_settings.txt
 
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
@@ -23,3 +33,4 @@ gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
 git config --global user.email "55552716+dylanuecker@users.noreply.github.com"
 git config --global user.name "Dylan Uecker"
 git config --global core.editor "vim"
+
