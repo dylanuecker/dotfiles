26f66bc554be5cb46a342e0505db9e832ec185fa
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-25 23:51:41 -0500

    Got polybar looking good and toggling and monocles and gaps and all that

--- config/polybar/toggle.sh
+++ config/polybar/toggle.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 if [ -f /tmp/polybarhidden ]; then 
-  polybar-msg cmd show 
-  bspc config top_padding 45 
-  rm /tmp/polybarhidden 
+	polybar-msg cmd show 
+	bspc config top_padding 32
+	bspc config gapless_monocle false
+	rm /tmp/polybarhidden 
 else 
-  polybar-msg cmd hide 
-  bspc config top_padding 0 
-  touch /tmp/polybarhidden 
+	polybar-msg cmd hide 
+	bspc config top_padding 0 
+	bspc config gapless_monocle true
+	touch /tmp/polybarhidden 
 fi
