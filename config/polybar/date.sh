95dd29c113c14ddd6162b83115434d95253a2f86
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-26 03:04:55 -0500

    Too tired ... just committing and sleeping

--- config/polybar/date.sh
+++ config/polybar/date.sh
@@ -4,7 +4,7 @@ t=0
 sleep_pid=0
 
 toggle() {
-	t=$(((t + 1) % 2))
+	t=$(((t + 1) % 3))
 
 	if [ "$sleep_pid" -ne 0 ]; then
 		kill $sleep_pid >/dev/null 2>&1
@@ -15,6 +15,8 @@ trap "toggle" USR1
 
 while true; do
 	if [ $t -eq 0 ]; then
+		date +%-l:%M\ %p
+	elif [ $t -eq 1 ]; then
 		date +%-m/%-d/%Y\ %-l:%M\ %p
 	else
 		date +%-m/%-d/%Y\ %-l:%M:%S\ %p
--- config/polybar/toggle.sh
+++ config/polybar/toggle.sh
@@ -2,11 +2,11 @@
 if [ -f /tmp/polybarhidden ]; then 
 	polybar-msg cmd show 
 	bspc config top_padding 32
-	bspc config gapless_monocle false
+	#bspc config gapless_monocle false
 	rm /tmp/polybarhidden 
 else 
 	polybar-msg cmd hide 
 	bspc config top_padding 0 
-	bspc config gapless_monocle true
+	#bspc config gapless_monocle true
 	touch /tmp/polybarhidden 
 fi
