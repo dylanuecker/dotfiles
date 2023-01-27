ecf64ec5777217f9627c92b519c86a96364433be
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-27 01:15:40 -0500

    Battery indicator good?

--- config/polybar/sound.sh
+++ config/polybar/sound.sh
@@ -7,15 +7,15 @@ arr=($vol)
 if [ ${#arr[*]} -eq 3 ]; then # array has len 3
 	echo ﱝ # muted volume
 	exit
-fi
-
-dec=${arr[1]}
-tens=${dec:2:1}
-
-if [ $tens -lt 3 ]; then
-	echo 奄
-elif [ $tens -lt 6 ]; then
-	echo 奔
 else
-	echo 墳
+	dec=${arr[1]}
+	tens=${dec:2:1}
+
+	if [ $tens -lt 3 ]; then
+		echo 奄
+	elif [ $tens -lt 6 ]; then
+		echo 奔
+	else
+		echo 墳
+	fi
 fi
