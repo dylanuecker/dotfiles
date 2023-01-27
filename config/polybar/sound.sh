12b3a3ed3699118235c29e386cea737099e517f0
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-26 23:41:04 -0500

    [Sound]s good

---
+++ config/polybar/sound.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+# example output: "Volume: 0.30 [MUTED]"
+vol=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+arr=($vol)
+
+if [ ${#arr[*]} -eq 3 ]; then # array has len 3
+	echo ﱝ # muted volume
+	exit
+fi
+
+dec=${arr[1]}
+tens=${dec:2:1}
+
+if [ $tens -lt 3 ]; then
+	echo 奄
+elif [ $tens -lt 6 ]; then
+	echo 奔
+else
+	echo 墳
+fi
