57fac561189f2fcfa7d6986d82bde3a7dfc3613b
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-28 21:20:30 -0500

    Polybar can be toggled per workspace and open-rofi better written, also with power menu being placed in the center

--- config/polybar/toggle.sh
+++ config/polybar/toggle.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
-if [ -f /tmp/polybarhidden ]; then 
+
+show() {
 	polybar-msg cmd show 
 	bspc config top_padding 21
 	#bspc config gapless_monocle false
-	rm /tmp/polybarhidden 
-else 
+}
+
+hide () {
 	polybar-msg cmd hide 
 	bspc config top_padding 0 
 	#bspc config gapless_monocle true
-	touch /tmp/polybarhidden 
+}
+
+ws=$(bspc query -D -d focused --names)
+
+# there has to a better logical solution to this, but
+# just want to get working and be done with it
+if [ "$1" = "No touchie." ]; then
+	if [ -f /tmp/polybarhidden$ws ]; then 
+		hide
+	else 
+		show
+	fi
+else
+	if [ -f /tmp/polybarhidden$ws ]; then 
+		show
+		rm /tmp/polybarhidden$ws 
+	else 
+		hide
+		touch /tmp/polybarhidden$ws
+	fi
 fi
