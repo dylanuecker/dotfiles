0b87eeb0356093aafda7b2c5408bb9d8cf106b55
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-01-29 02:49:37 -0500

    Auto start firefox on second workspace

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -3,5 +3,10 @@
 # Terminate already running bar instances
 killall -q polybar
 
+# hide polybar on every workspace except for 1 by default
+for i in {2..5}; do
+	touch /tmp/polybarhidden$i
+done
+
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 polybar dat 2>&1 | tee -a /tmp/polybar.log & disown
