281317de88370e079b152f5ae54d61ce143b6210
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-04-19 06:30:59 -0400

    Bedtime!

--- update_issue.sh
+++ update_issue.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-text="$(fortune -a | fmt -80 -s)"
+#text="$(fortune -a | fmt -200 -s)"
+#
+#issue="$(cowthink -f snoopy-doghouse <<< $text)"
+#issue+=$'\n' # new line between username enter
+#
+#echo "$issue" > /etc/issue
 
-issue="$(cowthink -f snoopy-doghouse <<< $text)"
-issue+=$'\n' # new line between username enter
+issue="\e{blue}$(fortune -a)\e{reset}" 
+issue+=$'\n'
 
 echo "$issue" > /etc/issue
-echo "$issue"
