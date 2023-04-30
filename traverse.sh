362a223ae134e244393cac26b0c4e53d811f3b00
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-04-30 03:45:15 -0400

    Better behavior, more to do

--- traverse.sh
+++ traverse.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 # TODO
-# do something when directory is empty
-# try to update PS1 in some way on each cd
-# what if number of directories carries over onto more than one line
+# - use stack property to keep track of last indices when moving up and down
+#   between directory levels, can "return" to previously selected dirs this
+#   way, won't be very hard
+# - try to update PS1 in some way on each cd
+# - what if number of directories carries over onto more than one line
 
 cleanup() {
 	tput el
@@ -23,7 +25,11 @@ while : ; do
 	dirs=($(ls -d */ 2> /dev/null | tr -d "/"))
 	output=($(ls --color=always -d */ 2> /dev/null | tr -d "/"))
 	output[$index]="\033[31;1m${dirs[$index]}\033[0m"
-	echo -en "${output[*]}"
+	if [ ${#dirs[@]} == 0 ]; then
+		echo -n "no dirs"
+	else
+		echo -en "${output[*]}"
+	fi
 	tput el
 	echo -en "\r"
 	read -s -n 1 input
@@ -64,11 +70,7 @@ while : ; do
 			index=0
 			;;
 		"")
-			cd ${dirs[$index]} 
 			break
-			;;
-		*) 
-			break 
 			;;
 	esac
 done
