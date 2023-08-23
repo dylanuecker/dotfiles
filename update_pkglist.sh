2fa2f944dd56ef589671f011ddd8374e1e0205d2
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-08-23 15:12:10 -0400

    Handle between pc and laptop

--- update_pkglist.sh
+++ update_pkglist.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-cd /home/dylan/dotfiles
-pacman -Qqe > pkglist.txt
-if [[ 'git status --porcelain pkglist.txt' ]]; then
-	git add pkglist.txt
+if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	filename=laptop-pkglist
+else
+	filename=pc-pkglist
+fi
+
+cd /home/dylan/.private-dotfiles
+pacman -Qqe > $filename
+if [[ 'git status --porcelain $filename' ]]; then
+	git add $filename
 	git commit -m "Update package list"
+	git push
 fi
