f83e0d8ec38041b7934e358ca3ae6b592d6d8697
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-14 15:22:55 -0400

    Using python script for noaa api instead

---
+++ wayland/.config/waybar/weather.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import requests
+
+# uses the national weather service api
+data = requests.get('https://location.services.mozilla.com/v1/geolocate?key=geoclue').json()['location']
+url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast']
+temp = requests.get(url).json()['properties']['periods'][0]['temperature']
+print(str(temp) + "°")
