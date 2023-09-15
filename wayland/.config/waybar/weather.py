002fc0acbc0c805e72195bfb3a21b7e1d5c0cb19
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-14 23:50:10 -0400

    Use hourly forecast

--- wayland/.config/waybar/weather.py
+++ wayland/.config/waybar/weather.py
@@ -3,6 +3,6 @@ import requests
 
 # uses the national weather service api
 data = requests.get('https://location.services.mozilla.com/v1/geolocate?key=geoclue').json()['location']
-url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast']
+url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast'] + "/hourly"
 temp = requests.get(url).json()['properties']['periods'][0]['temperature']
 print(str(temp) + "°")
