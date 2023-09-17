9403ecbfed55741f62df2a2e82e85cf967b15cea
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-17 12:08:46 -0400

    Handle pc vs laptop cpu

--- wayland/.config/waybar/cputemp.sh
+++ wayland/.config/waybar/cputemp.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-
-temp=$(sensors k10temp-pci-00c3 | grep Tctl | awk '{print $2}' | cut -c 2-3)
+if [[ $HOSTNAME == "cloudbreak" ]]; then
+	temp=$(sensors coretemp-isa-0000 | grep "Package id 0" | awk '{print $4}' | cut -c 2-3)
+else
+	temp=$(sensors k10temp-pci-00c3 | grep Tctl | awk '{print $2}' | cut -c 2-3)
+fi
 echo "CPU $temp°C "
