cdbb28d31ba370e2531587a5fe55a6ec42b5c884
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-17 14:08:14 -0400

    Moving to stats.sh

--- wayland/.config/waybar/cputemp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [[ $HOSTNAME == "cloudbreak" ]]; then
-	temp=$(sensors coretemp-isa-0000 | grep "Package id 0" | awk '{print $4}' | cut -c 2-3)
-else
-	temp=$(sensors k10temp-pci-00c3 | grep Tctl | awk '{print $2}' | cut -c 2-3)
-fi
-echo "CPU $temp°C "
--- wayland/.config/waybar/gpustats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-temp=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
-util=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
-if [[ $? == 0 ]]; then
-	echo "GPU $temp°C $(printf "%2d" $util)%"
-fi
