58abc877bdc443cdca32db7a7a0dd2475853d6a9
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-18 12:15:32 -0400

    Lock laptop how I like

--- wayland/.config/swaylock/lock.sh
+++ wayland/.config/swaylock/lock.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 image=/home/dylan/.config/lockscreen
 
+if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	hyprctl dispatch dpms off
+fi
+
 grim $image
-convert $image -blur 0x6 $image
-swaylock
+convert $image -blur 0x6 $image 
+
+if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	sleep 0.5; (sleep 0.5; hyprctl dispatch dpms on) &
+fi
+
+swaylock; killall -SIGUSR2 waybar
