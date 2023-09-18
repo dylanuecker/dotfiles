c63c61b8575677b1b6f09b6670a3228a9b760efd
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-18 11:45:57 -0400

    There is no try

--- wayland/.config/waybar/weather.py
+++ wayland/.config/waybar/weather.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-import requests
+import requests, time
 
-# uses the national weather service api
-data = requests.get('https://location.services.mozilla.com/v1/geolocate?key=geoclue').json()['location']
-url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast'] + "/hourly"
-temp = requests.get(url).json()['properties']['periods'][0]['temperature']
-print(str(temp) + "°")
+def do_or_do_not():
+    try:
+        # uses the national weather service api
+        data = requests.get('https://location.services.mozilla.com/v1/geolocate?key=geoclue').json()['location']
+        url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast'] + "/hourly"
+        temp = requests.get(url).json()['properties']['periods'][0]['temperature']
+        print(str(temp) + "°")
+    except:
+        time.sleep(1)
+        do_or_do_not()
+
+do_or_do_not()
