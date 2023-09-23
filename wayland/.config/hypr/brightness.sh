faf2683e3ab3167e8906862e7a0c9546088c80ec
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-23 10:11:56 -0400

    Fix typos

--- wayland/.config/hypr/brightness.sh
+++ wayland/.config/hypr/brightness.sh
@@ -6,6 +6,6 @@ elif [ $1 = 'down' ]; then
 	brightnessctl set --min-value=12010 10-%
 fi
 
-brightness=$(brightnessctl | awk '/Current { print $4 }' | tr -d "()"
+brightness=$(brightnessctl | awk '/Current/ { print $4 }' | tr -d "()")
 notify-send "Brightness $brightness" \
 	-t 1500 -h string:x-canonical-private-synchronous:brightness-notification
