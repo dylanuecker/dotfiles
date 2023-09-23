02ca9a9ba538a388459d59ecbc576ba2140d8fed
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-09-23 10:09:17 -0400

    Send notification when brightness changes

---
+++ wayland/.config/hypr/brightness.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $1 = 'up' ]; then
+	brightnessctl set --min-value=12010 10+%
+elif [ $1 = 'down' ]; then
+	brightnessctl set --min-value=12010 10-%
+fi
+
+brightness=$(brightnessctl | awk '/Current { print $4 }' | tr -d "()"
+notify-send "Brightness $brightness" \
+	-t 1500 -h string:x-canonical-private-synchronous:brightness-notification
