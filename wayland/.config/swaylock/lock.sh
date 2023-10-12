5e8adbad0a62f1aafcc08b6a4b011d4c672c3ef5
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2023-10-11 21:26:40 -0400

    Works with dual monitors

--- wayland/.config/swaylock/lock.sh
+++ wayland/.config/swaylock/lock.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-image=/home/dylan/.config/lockscreen
+imagea=/home/dylan/.config/lockscreena
+imageb=/home/dylan/.config/lockscreenb
 
 hyprctl keyword general:cursor_inactive_timeout 1
 
-grim $image
-
 if [[ "$HOSTNAME" == "cloudbreak" ]]; then
+	grim $imagea
 	hyprctl dispatch dpms off
+else
+	grim -o DP-1 $imagea
+	grim -o HDMI-A-2 $imageb
+	convert $imageb -blur 0x6 $imageb
 fi
 
-convert $image -blur 0x6 $image 
+convert $imagea -blur 0x6 $imagea 
 
 if [[ "$HOSTNAME" == "cloudbreak" ]]; then
 	sleep 0.5; (sleep 0.5; hyprctl dispatch dpms on) &
+	images="--image $imagea"
+else
+	images="--image DP-1:$imagea --image HDMI-A-2:$imageb"
 fi
 
-swaylock; hyprctl keyword general:cursor_inactive_timeout 0; killall -SIGUSR2 waybar
+swaylock $images; hyprctl keyword general:cursor_inactive_timeout 0; killall -SIGUSR2 waybar
