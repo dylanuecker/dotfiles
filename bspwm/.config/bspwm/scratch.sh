189e10b3a891c6aa9b609445723b725fc2fba87a
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-01-24 12:06:05 -0500

    Two scratchpads

--- bspwm/.config/bspwm/scratch.sh
+++ bspwm/.config/bspwm/scratch.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
-# only add floating scratchpad window node id to /tmp/scratchid
-bspc query -N -n .floating | xargs -i sh -c 'bspc query  --node {} -T | grep -q scratchpad && echo {} > /tmp/scratchid'
+# $1 is scratchpad "index" number
+#
+# add floating scratchpad window node id to /tmp/scratchid$1
+bspc query -N -n .floating | xargs -i sh -c "bspc query  --node {} -T | grep -q scratchpad$1 && echo {} > /tmp/scratchid$1"
 exec $SHELL
