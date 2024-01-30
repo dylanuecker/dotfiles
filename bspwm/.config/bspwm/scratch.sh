0b3d9da2b66aa57f572ffa6e3c1eb253e73249fa
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-01-30 14:19:22 -0500

    Reopen scratchpads if they close

--- bspwm/.config/bspwm/scratch.sh
+++ bspwm/.config/bspwm/scratch.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/sh
 # $1 is scratchpad "index" number
+# $2 is unhide at start
 #
 # add floating scratchpad window node id to /tmp/scratchid$1
 bspc query -N -n .floating | xargs -i sh -c "bspc query  --node {} -T | grep -q scratchpad$1 && echo {} > /tmp/scratchid$1"
+if [ $# -eq 2 ]; then
+    id=$(cat /tmp/scratchid$1)
+    bspc node $id --flag hidden
+    bspc node -f $id
+fi
 exec $SHELL
---
+++ bspwm/.config/bspwm/start_scratch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+# $1 is scratchpad "index" number
+# $2 is unhide at start
+#
+# check if scratchpad is running, if not start it
+[ "$(ps -x | grep -c "scratchpad$1")" -eq "1" ] && kitty --class scratchpad$1 ~/.config/bspwm/scratch.sh $1 $2 &
