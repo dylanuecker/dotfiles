be5f0e3ceec4e621b62cfcf804cebe914da27130
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-06-26 23:19:33 -0400

    Usable setup

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    source ~/.env
+    source ~/.alias # TODO turn some of these into abbreviations?
+
+    fish_vi_key_bindings insert
+    bind -M insert -m default jk cancel repaint-mode
+    set -g fish_sequence_key_delay_ms 200
+
+    fzf_key_bindings
+
+    # TODO dirstack? get cd - working
+    # TODO pager color variables
+    # TODO LIST_TYPES like option?
+
+    # TODO better prompt
 end
