0a2562c25e2d0f15426c8270481ca83ee0f27fc8
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-07-29 22:53:49 -0400

    Make minimal prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,14 +1,15 @@
 function fish_prompt --description 'Left prompt'
     set -l last_pipestatus $pipestatus
-
     set -l pipestatus_string (__fish_print_pipestatus " [" "]" "|" (set_color --bold $fish_color_status) \
                              (set_color --bold $fish_color_status) $last_pipestatus)
 
-    printf '%s%s@%s %s%s%s%s%s%s ' \
-        (set_color --bold brblack) $USER (prompt_hostname) \
+    set -l git_prompt_output (string replace -ra '\(|\)' '' (fish_git_prompt))
+
+    printf '%s%s%s%s%s%s%s' \
         (set_color --bold blue) (prompt_path) \
-        (set_color --bold magenta) (fish_vcs_prompt) \
+        (set_color --bold brblack) $git_prompt_output \
         $pipestatus_string \
+        (set_color --bold yellow) " > "\
         (set_color normal)
 end
 
