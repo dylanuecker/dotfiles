3166b34018bc64ad944f2462582e99050aaf65c5
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-09-04 17:33:26 -0400

    QoL improvements

--- bspwm/.config/bspwm/start_scratch.sh
+++ bspwm/.config/bspwm/start_scratch.sh
@@ -3,4 +3,4 @@
 # $2 is unhide at start
 #
 # check if scratchpad is running, if not start it
-[ "$(ps -x | grep -c "scratchpad$1")" -eq "1" ] && kitty --class scratchpad$1 ~/.config/bspwm/scratch.sh $1 $2 &
+[ "$(ps -x | grep -c "scratchpad$1")" -eq "1" ] && kitty --override background_opacity=1 --class scratchpad$1 ~/.config/bspwm/scratch.sh $1 $2 &
