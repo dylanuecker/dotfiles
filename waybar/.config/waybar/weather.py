a5ee47c5b5852a069aaaa44b44652ab9493ef9e7
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-11-09 17:57:04 -0500

    Replace MLS with ipinfo

--- waybar/.config/waybar/weather.py
+++ waybar/.config/waybar/weather.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
+
 import requests, time
 
-def do_or_do_not():
-    try:
-        # uses the national weather service api
-        data = requests.get('https://location.services.mozilla.com/v1/geolocate?key=geoclue').json()['location']
-        url = requests.get(f'https://api.weather.gov/points/{data["lat"]},{data["lng"]}').json()['properties']['forecast'] + "/hourly"
-        temp = requests.get(url).json()['properties']['periods'][0]['temperature']
-        print(str(temp) + "°")
-    except:
-        time.sleep(1)
-        do_or_do_not()
-
-do_or_do_not()
+token = open('.apitoken', 'r').readline().rstrip()
+coords = requests.get(f'https://ipinfo.io?token={token}').json()['loc']
+lat,lng = coords.split(',')
+url = requests.get(f'https://api.weather.gov/points/{lat},{lng}').json()['properties']['forecast'] + "/hourly"
+temp = requests.get(url).json()['properties']['periods'][0]['temperature']
+print(str(temp) + "°")
