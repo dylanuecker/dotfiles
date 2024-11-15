ba99349c17ea81109aeed8d97593b6d93da5a2e8
Dylan Uecker <55552716+dylanuecker@users.noreply.github.com>
2024-11-14 21:58:48 -0500

    Add some basic error handling

--- waybar/.config/waybar/weather.py
+++ waybar/.config/waybar/weather.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
-import requests, time
+import os
+import requests
 
-token = open('.apitoken', 'r').readline().rstrip()
-coords = requests.get(f'https://ipinfo.io?token={token}').json()['loc']
-lat,lng = coords.split(',')
-url = requests.get(f'https://api.weather.gov/points/{lat},{lng}').json()['properties']['forecast'] + "/hourly"
-temp = requests.get(url).json()['properties']['periods'][0]['temperature']
-print(str(temp) + "°")
+def get_current_temp(file):
+    try:
+        token = open(file, 'r').readline().rstrip()
+        coords = requests.get(f'https://ipinfo.io?token={token}').json()['loc']
+        lat,lng = coords.split(',')
+        url = requests.get(f'https://api.weather.gov/points/{lat},{lng}').json()['properties']['forecastHourly']
+        temp = requests.get(url).json()['properties']['periods'][0]['temperature']
+        print(str(temp) + "°")
+    except:
+        pass
+
+file = os.path.join(os.path.dirname(__file__), '.apitoken')
+if os.path.exists(file):
+    get_current_temp(file)
